b5d6c743319ed1c46485b18f2855d771a8653272
cosmicbuffalo <cosmic.buffalo.dev@gmail.com>
2017-04-25 13:55:09 -0500

    started adding more icons and preparing for AJAX functionality

--- apps/main/static/main/js/home_page.js
+++ apps/main/static/main/js/home_page.js
@@ -21,63 +21,60 @@ $ (document).ready(function(){
 
 
   //file upload AJAX
-  $('#file-upload-button').on('click',function(){
-
-      event.preventDefault()
-      $.ajax({
-          url: $('#file-upload-form').attr('action'),
-          method: 'post',
-          data: data,
-          cache: false,
-          processData: false,
-          contentType: false,
-          success: function(response){
-              console.log("Entered success function, Response is ...")
-              console.log(response)
-
-              htmlString = ""
-              htmlString += "<tr class='file-flash'><td>"
-              htmlString += response.file_data
-              htmlString += "</td><td>"
-              htmlString += response.owner_name
-              htmlString +="</td><td>"
-              htmlString += response.updated_at
-              htmlString +="</td><td>"
-              htmlString += response.file_size
-              htmlString +="</td></tr>"
-
-              $('#table-body').append(htmlString)
-          }
-      })
-  })
+  // $('#file-upload-button').on('click',function(){
+  //
+  //     event.preventDefault()
+  //     $.ajax({
+  //         url: $('#file-upload-form').attr('action'),
+  //         method: 'post',
+  //         data: data,
+  //         cache: false,
+  //         processData: false,
+  //         contentType: false,
+  //         success: function(response){
+  //             console.log("Entered success function, Response is ...")
+  //             console.log(response)
+  //
+  //             htmlString = ""
+  //             htmlString += "<tr class='file-flash'><td>"
+  //             htmlString += response.file_data
+  //             htmlString += "</td><td>"
+  //             htmlString += response.owner_name
+  //             htmlString +="</td><td>"
+  //             htmlString += response.updated_at
+  //             htmlString +="</td><td>"
+  //             htmlString += response.file_size
+  //             htmlString +="</td></tr>"
+  //
+  //             $('#table-body').append(htmlString)
+  //         }
+  //     })
+  // })
 
 
 
   //tool bar button functionality
-  // $('.tool-bar-link').on('click', function(){
-  //
-  //   $.post({
-  //     url:$(this).attr('href'),
-  //     data:JSON.stringify($('.selected'))
-  //
-  //
-  //   })
+  $('.tool-bar-link').on('click', function(){
 
+    event.preventDefault()
 
+    console.log($('.selected'))
 
-    //         }
-
-
-    //     })
-
-
-
-
+    // $.post({
+    //   url:$(this).attr('href'),
+    //   data:JSON.stringify($('.selected'))
+    //
+    //
     // })
 
 
 
+
+
+
   })
+
+
 
 
 

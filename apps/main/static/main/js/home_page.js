f2e6ef52495f07ca48f5a60ab330b03d751fc347
Armando <armando.amador@hotmail.com>
2017-04-28 12:52:00 -0500

    Ajax uploads wooooo

--- apps/main/static/main/js/home_page.js
+++ apps/main/static/main/js/home_page.js
@@ -1,15 +1,15 @@
-$ (document).ready(function(){
+$(document).ready(function () {
 
-//---------------------------
-//------ TOOLBAR STUFF ------
-//---------------------------
+  //---------------------------
+  //------ TOOLBAR STUFF ------
+  //---------------------------
 
-  $('#preview-button').on('click', function(){
+  $('#preview-button').on('click', function () {
     console.log("Clicked preview button")
     var rows = $('tr.selected')
     console.log(rows)
     console.log(rows.length)
-    if (rows.length == 1){
+    if (rows.length == 1) {
       console.log(rows[0])
       img = $('tr.selected :first-child div img')
       console.log(img)
@@ -17,7 +17,7 @@ $ (document).ready(function(){
     }
   })
 
-  $('#share-button').on('click', function(){
+  $('#share-button').on('click', function () {
 
 
 
@@ -25,43 +25,43 @@ $ (document).ready(function(){
     $('#share-modal').modal();
   })
 
-  $('#move_to_trash').on('click', function(){
+  $('#move_to_trash').on('click', function () {
 
-    var post_data ={}
+    var post_data = {}
     var num_rows = 0
     event.preventDefault()
-    for(var i = 0; i < $('table .selected').length; i=i+1){
-        console.log($('table .selected')[i].attributes)
+    for (var i = 0; i < $('table .selected').length; i = i + 1) {
+      console.log($('table .selected')[i].attributes)
 
-        var id = ($('table .selected')[i].attributes[1].value)
-        var type = ($('table .selected')[i].attributes[2].value)
+      var id = ($('table .selected')[i].attributes[1].value)
+      var type = ($('table .selected')[i].attributes[2].value)
 
 
-        post_data[i]= {"type": type,'id': id,}
-        num_rows +=1
+      post_data[i] = { "type": type, 'id': id, }
+      num_rows += 1
     }
 
 
     post_data['csrfmiddlewaretoken'] = document.getElementsByName('csrfmiddlewaretoken')[0].value
     post_data["num_rows"] = num_rows
     $.post({
-        url:'/move_to_trash',
-        data: post_data,
-        dataType:'json',
-        success: function(res){
+      url: '/move_to_trash',
+      data: post_data,
+      dataType: 'json',
+      success: function (res) {
 
-          $('table .selected').delay(400).slideToggle(400).delay(500)
-          $('table .selected').delay(400).remove()
+        $('table .selected').delay(400).slideToggle(400).delay(500)
+        $('table .selected').delay(400).remove()
 
-            // console.log(res)
-            // $.get({
-            //     url: "/folder_body/" + res.current_folder,
-            //     success:function(new_html){
-            //         replaceTableBody(new_html)
-            //         // assignSelectionHandler();
-            //     }
-            // })
-        }
+        // console.log(res)
+        // $.get({
+        //     url: "/folder_body/" + res.current_folder,
+        //     success:function(new_html){
+        //         replaceTableBody(new_html)
+        //         // assignSelectionHandler();
+        //     }
+        // })
+      }
 
 
     })
@@ -76,55 +76,55 @@ $ (document).ready(function(){
 
 
   $('#account-dropdown-button').dropdown({
-      inDuration: 100,
-      outDuration: 100,
-      constrainWidth: false, // Does not change width of dropdown to that of the activator
-      hover: false, // Activate on hover
-      gutter: 14, // Spacing from edge
-      belowOrigin: true, // Displays dropdown below the button
-      alignment: 'left', // Displays dropdown with edge aligned to the left of button
-      stopPropagation: false // Stops event propagation
-    }
+    inDuration: 100,
+    outDuration: 100,
+    constrainWidth: false, // Does not change width of dropdown to that of the activator
+    hover: false, // Activate on hover
+    gutter: 14, // Spacing from edge
+    belowOrigin: true, // Displays dropdown below the button
+    alignment: 'left', // Displays dropdown with edge aligned to the left of button
+    stopPropagation: false // Stops event propagation
+  }
   );
   $('#new-button').dropdown({
-      inDuration: 100,
-      outDuration: 100,
-      constrainWidth: false, // Does not change width of dropdown to that of the activator
-      hover: false, // Activate on hover
-      gutter: 14, // Spacing from edge
-      belowOrigin: true, // Displays dropdown below the button
-      alignment: 'left', // Displays dropdown with edge aligned to the left of button
-      stopPropagation: false // Stops event propagation
-    }
+    inDuration: 100,
+    outDuration: 100,
+    constrainWidth: false, // Does not change width of dropdown to that of the activator
+    hover: false, // Activate on hover
+    gutter: 14, // Spacing from edge
+    belowOrigin: true, // Displays dropdown below the button
+    alignment: 'left', // Displays dropdown with edge aligned to the left of button
+    stopPropagation: false // Stops event propagation
+  }
   );
 
 
-  $('#file-upload-button').on('click', function(){
+  $('#file-upload-button').on('click', function () {
     // event.preventDefault()
     console.log("Clicked file upload button")
     $('#file-input').trigger('click');
   })
   assignFileUploadHandler();
 
-  $('#new-folder-button').on('click', function(){
-        $('#modal1').modal();
+  $('#new-folder-button').on('click', function () {
+    $('#modal1').modal();
   })
 
 
-  $('#account-dropdown.dropdown-content').on('click', function(){
+  $('#account-dropdown.dropdown-content').on('click', function () {
     event.stopPropagation();
   })
 
-  $('#sign-out-button').on('click', function(){
+  $('#sign-out-button').on('click', function () {
     post_data = {}
     post_data['csrfmiddlewaretoken'] = document.getElementsByName('csrfmiddlewaretoken')[0].value
     $.post({
-      url:$(this).attr('href'),
-      dataType:'json',
-      data:post_data,
-      success:function(res){
+      url: $(this).attr('href'),
+      dataType: 'json',
+      data: post_data,
+      success: function (res) {
         console.log(res)
-        if (res.redirect !== undefined && res.redirect){
+        if (res.redirect !== undefined && res.redirect) {
           window.location.href = res.redirect_url
         }
       }
@@ -137,7 +137,7 @@ $ (document).ready(function(){
   //------------------------------
 
 
-  $('#left-bar-tab-menu li').on('click', function(){
+  $('#left-bar-tab-menu li').on('click', function () {
 
     console.log("clicked left bar tab")
     console.log($(this))
@@ -146,11 +146,11 @@ $ (document).ready(function(){
 
   })
 
-  $('#my-drive-tab').on('click', function(){
+  $('#my-drive-tab').on('click', function () {
     console.log("Clicked my drive")
     $.get({
       url: $(this).attr('href'),
-      success:function(res){
+      success: function (res) {
         // console.log(res)
         $('#breadcrumb-div').html('<a class="breadcrumb first-crumb" href="#"><span class="crumb-hover">My Drive</span></a>')
         replaceTableBody(res)
@@ -158,14 +158,14 @@ $ (document).ready(function(){
       }
     })
   })
-    $('#trash-tab').on('click', function(){
-        $.get({
-            url: $(this).attr('href'),
-            success: function(res){
-              replaceTableBody(res)
-            }
-        })
+  $('#trash-tab').on('click', function () {
+    $.get({
+      url: $(this).attr('href'),
+      success: function (res) {
+        replaceTableBody(res)
+      }
     })
+  })
 
 
   //---------------------------
@@ -174,76 +174,76 @@ $ (document).ready(function(){
 
 
   var lastSelectedRow;
-  $('#table-body').on('click', 'tr.item-row', function(){
-      var rows = $('#tbody-content-table tr')
-      console.log("triggered row selection handler")
-      RowClick($(this),false,rows)
-      $('#selection-tools').css('display', 'inline-block')
-      var selected = $('tr.selected')
-      if (selected.length == 1){
-        // console.log("There is one row selected")
-        selected_type = $(selected[0])[0].attributes['item-type'].value
-        // console.log(selected_type)
-        if (selected_type != "folder"){
-          $('#preview-button').css('display', 'inline-block')
-        }else{
-          $('#preview-button').css('display', 'none')
-        }
-      } else{
-        // console.log("there are... " + String(selected.length) + " rows selected")
+  $('#table-body').on('click', 'tr.item-row', function () {
+    var rows = $('#tbody-content-table tr')
+    console.log("triggered row selection handler")
+    RowClick($(this), false, rows)
+    $('#selection-tools').css('display', 'inline-block')
+    var selected = $('tr.selected')
+    if (selected.length == 1) {
+      // console.log("There is one row selected")
+      selected_type = $(selected[0])[0].attributes['item-type'].value
+      // console.log(selected_type)
+      if (selected_type != "folder") {
+        $('#preview-button').css('display', 'inline-block')
+      } else {
         $('#preview-button').css('display', 'none')
       }
-  })
-//-----------------------------
-//----TILED VIEW SELECTION-----
-//-----------------------------
-   var lastSelectedRow;
-  $('#table-body').on('click','div.tile', function(){
-      var tiles = $('.tile')
-      console.log("triggered row selection handler")
-      RowClick($(this),false,tiles)
-      $('#selection-tools').css('display', 'inline-block')
-      var selected = $('div.tile.selected')
-      if (selected.length == 1){
-        // console.log("There is one row selected")
-        // $(selected).css('background-color',"#5764f7")
-        selected_type = $(selected[0])[0].attributes['item-type'].value
-        // console.log(selected_type)
-        if (selected_type != "folder"){
-          $('#preview-button').css('display', 'inline-block')
-        }else{
-          $('#preview-button').css('display', 'none')
-        }
-      } else{
-        // console.log("there are... " + String(selected.length) + " rows selected")
-        $('#preview-button').css('display', 'none')
-      }
-  })
-
-
-//-----------------------------
-//--------- GRID VIEW ---------
-//-----------------------------
-$("#view-change-icon")
-
-$("#view-change-icon").on('click', function(){
-  // document.getElementById('#view-change-icon').src=$(this).attr("list-grey")
-   $.get({
-    url: $(this).attr('href'),
-    success: function(res){
-      $('#tbody-content-wrapper').delay(400).remove()
-      $.get({
-        url: "home/folder_body/"+res["current_folder"],
-        success: function(result){
-          replaceTableBody(result)
-
-        }
-      })
+    } else {
+      // console.log("there are... " + String(selected.length) + " rows selected")
+      $('#preview-button').css('display', 'none')
     }
-
-
   })
-})
+  //-----------------------------
+  //----TILED VIEW SELECTION-----
+  //-----------------------------
+  var lastSelectedRow;
+  $('#table-body').on('click', 'div.tile', function () {
+    var tiles = $('.tile')
+    console.log("triggered row selection handler")
+    RowClick($(this), false, tiles)
+    $('#selection-tools').css('display', 'inline-block')
+    var selected = $('div.tile.selected')
+    if (selected.length == 1) {
+      // console.log("There is one row selected")
+      // $(selected).css('background-color',"#5764f7")
+      selected_type = $(selected[0])[0].attributes['item-type'].value
+      // console.log(selected_type)
+      if (selected_type != "folder") {
+        $('#preview-button').css('display', 'inline-block')
+      } else {
+        $('#preview-button').css('display', 'none')
+      }
+    } else {
+      // console.log("there are... " + String(selected.length) + " rows selected")
+      $('#preview-button').css('display', 'none')
+    }
+  })
+
+
+  //-----------------------------
+  //--------- GRID VIEW ---------
+  //-----------------------------
+  $("#view-change-icon")
+
+  $("#view-change-icon").on('click', function () {
+    // document.getElementById('#view-change-icon').src=$(this).attr("list-grey")
+    $.get({
+      url: $(this).attr('href'),
+      success: function (res) {
+        $('#tbody-content-wrapper').delay(400).remove()
+        $.get({
+          url: "home/folder_body/" + res["current_folder"],
+          success: function (result) {
+            replaceTableBody(result)
+
+          }
+        })
+      }
+
+
+    })
+  })
 
   //---------------------------
   //---- FOLDER NAVIGATION ----
@@ -256,7 +256,7 @@ $("#view-change-icon").on('click', function(){
   // })
 
 
-  $('#breadcrumb-div').on('click', 'a.not-first', function(){
+  $('#breadcrumb-div').on('click', 'a.not-first', function () {
     event.preventDefault()
     console.log("You just clicked a breadcrumb")
     console.log($(this).attr('href'))
@@ -265,7 +265,7 @@ $("#view-change-icon").on('click', function(){
 
     $.get({
       url: $(this).attr('href'),
-      success:function(res){
+      success: function (res) {
         replaceTableBody(res)
         console.log(clicked)
         console.log(clicked.nextAll('a.breadcrumb'))
@@ -275,7 +275,7 @@ $("#view-change-icon").on('click', function(){
 
   })
 
-  $('#breadcrumb-div').on('click', '#first-crumb', function(){
+  $('#breadcrumb-div').on('click', '#first-crumb', function () {
     console.log("clicked first child")
     event.preventDefault()
     console.log($(this))
@@ -286,7 +286,7 @@ $("#view-change-icon").on('click', function(){
   })
 
 
-  $('#table-body').on('click','.folder-name-div a', function(){
+  $('#table-body').on('click', '.folder-name-div a', function () {
     console.log("You just clicked a folder link!")
     event.preventDefault()
     var folder_name = $(this)[0].innerText
@@ -296,7 +296,7 @@ $("#view-change-icon").on('click', function(){
 
     $.get({
       url: $(this).attr('href'),
-      success:function(res){
+      success: function (res) {
         replaceTableBody(res)
         $('#breadcrumb-div').append('<a class="breadcrumb not-first" href=' + href + '><span class="crumb-hover">' + folder_name + '</span></a>')
       }
@@ -306,7 +306,7 @@ $("#view-change-icon").on('click', function(){
   //Initialize first view of root folder
   $.get({
     url: $('#my-drive-tab').attr('href'),
-    success:function(res){
+    success: function (res) {
       // console.log(res)
       replaceTableBody(res)
       $('#breadcrumb-div').html('<a id="first-crumb" class="breadcrumb" href="#"><span class="crumb-hover">My Drive</span></a>')
@@ -333,75 +333,108 @@ $("#view-change-icon").on('click', function(){
 
 
 // disable text selection
-document.onselectstart = function() {
-    return false;
+document.onselectstart = function () {
+  return false;
 }
 
 function RowClick(currenttr, lock, rows) {
-    if (window.event.ctrlKey) {
-        toggleRow(currenttr);
+  if (window.event.ctrlKey) {
+    toggleRow(currenttr);
+  }
+
+  if (window.event.button === 0) {
+
+    if (!window.event.ctrlKey && !window.event.shiftKey) {
+      clearAll(rows);
+      toggleRow(currenttr);
     }
 
-    if (window.event.button === 0) {
-
-        if (!window.event.ctrlKey && !window.event.shiftKey) {
-            clearAll(rows);
-            toggleRow(currenttr);
-        }
-
-        if (window.event.shiftKey) {
-            selectRowsBetweenIndexes([lastSelectedRow.index(), currenttr.index()], rows)
-        }
+    if (window.event.shiftKey) {
+      selectRowsBetweenIndexes([lastSelectedRow.index(), currenttr.index()], rows)
     }
+  }
 }
 
 function toggleRow(row) {
-    if (!row.hasClass('header-row')){
-        if (row.hasClass('selected')){
-            row.removeClass('selected')
-            console.log (rows)
-        }
-        else{
-            row.addClass('selected')
+  if (!row.hasClass('header-row')) {
+    if (row.hasClass('selected')) {
+      row.removeClass('selected')
+      console.log(rows)
+    }
+    else {
+      row.addClass('selected')
 
-        }
-        lastSelectedRow = row;
-        }
+    }
+    lastSelectedRow = row;
+  }
 
 }
 
-function selectRowsBetweenIndexes(indexes,rows) {
-    indexes.sort(function(a, b) {
-        return a - b;
-    });
+function selectRowsBetweenIndexes(indexes, rows) {
+  indexes.sort(function (a, b) {
+    return a - b;
+  });
 
-    for (var i = indexes[0]; i <= indexes[1]; i++) {
-        $(rows[i+1]).addClass('selected');
-    }
+  for (var i = indexes[0]; i <= indexes[1]; i++) {
+    $(rows[i + 1]).addClass('selected');
+  }
 }
 
 function clearAll(rows) {
-    for (var i = 0; i < rows.length; i++) {
-        $(rows[i]).removeClass("selected");
-    }
+  for (var i = 0; i < rows.length; i++) {
+    $(rows[i]).removeClass("selected");
+  }
 }
 
-function replaceTableBody(new_html){
+function replaceTableBody(new_html) {
   console.log("replacing html...")
   $('#selection-tools').fadeOut(300)
-  $('#table-body').fadeOut(300, function(){
+  $('#table-body').fadeOut(300, function () {
     $('#table-body').html(new_html)
-    $('#table-body').delay(100).fadeIn(300, function(){
+    $('#table-body').delay(100).fadeIn(300, function () {
       console.log('done')
       $('.materialboxed').materialbox();
       assignFileUploadHandler()
+      $('#file-upload-form').ajaxForm({
+        success: function (res) {
+          console.log(res)
+          alert("File Upload Success!");
+          $.get({
+            url: "home/folder_body/" + res["folder_id"],
+            success: function (result) {
+              replaceTableBody(result)
 
+            }
+          })
+
+
+
+        }
+      })
+      $('#folder-create-form').ajaxForm({
+        success: function (res) {
+          console.log(res)
+          alert("Folder Creation Success!");
+          $('#modal1').modal('close')
+          $.get({
+            url: "home/folder_body/" + res["folder_id"],
+            success: function (result) {
+              replaceTableBody(result)
+
+
+            }
+          })
+
+
+
+        }
+      })
     })
   })
 }
 
-function assignFileUploadHandler(){
-  $('#file-input').on('change', function(){
+function assignFileUploadHandler() {
+  $('#file-input').on('change', function () {
     console.log("triggered file input on change")
     $('#file-upload-form').submit();
   })
